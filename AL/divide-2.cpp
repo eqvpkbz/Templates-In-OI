f53a91c1229b805b4102771b0a860134e3ef1e37
Eqvpkbz <33982692+eqvpkbz@users.noreply.github.com>
2020-12-04 15:24:59 +0800

    Update divide-2.cpp

--- AL/divide-2.cpp
+++ AL/divide-2.cpp
@@ -1,92 +1,70 @@
-#include <cstdio> 
+#include <cstdio>
+
+#define re register
+#define rep(i, a, b) for(re int i = (a); i <= (b); ++ i)
 
 static inline int rd() {
-    int res = 0; char c = getchar();
+    re int res = 0; char c = getchar(); 
     while (c < 48 || c > 57) c = getchar();
     while (c >= 48 && c <= 57) res = res * 10 + c - 48, c = getchar();
-    return res;
+    return res; 
 }
 
 const int N = 1e6 + 5;
 const int INF = 0x3f3f3f3f;
 
-int n,m,cnt;
-struct Query
-{
-	int x,y,k;
-	int pos,type;
-	Query(){}
-	Query(int i,int j,int kk,int p,int t):x(i),y(j),k(kk),pos(p),type(t){}
-}q[N],q1[N],q2[N];
-int ans[N],c[N];
-inline int lowbit(int x){return x&-x;}
-inline void add(int x,int v)
-{
-	for(;x<=n;x+=lowbit(x)) c[x]+=v;
+int n, m, cnt;
+struct QUERY {
+    int l, r, k, pos, opt; 
+} q[N], q1[N], q2[N];
+
+int ans[N], c[N];
+
+static inline void add(int x, int val) {
+    while (x <= n) c[x] += val, x += x & -x;
+    return ;
 }
-inline int sum(int x)
-{
-	int res=0;
-	for(;x;x-=lowbit(x)) res+=c[x];
-	return res;
+
+static inline int ask(int x) {
+    re int res = 0; while (x)
+        res += c[x], x -= x & -x; 
+    return res; 
 }
-void solve(int l,int r,int ql,int qr)
-{
-	if(l>r||ql>qr) return;
-    printf("solving %d %d %d %d\n", l, r, ql, qr);
-    for(int i = ql; i <= qr; ++ i)
-            printf("%d [%d, %d] %d %d\n", q[i].type, q[i].x, q[i].y, q[i].pos, q[i].k); 
-	if(l==r)
-	{
-		for(int i=ql;i<=qr;i++)
-			if(q[i].type) ans[q[i].pos]=l;
-		return;
-	}
-	int cnt1=0,cnt2=0;
-	int mid=(l+r)>>1;
-	for(int i=ql;i<=qr;i++)
-		if(q[i].type)
-		{
-			int tmp=sum(q[i].y)-sum(q[i].x-1);
-			if(q[i].k<=tmp) q1[++cnt1]=q[i];
-			else q[i].k-=tmp,q2[++cnt2]=q[i];
-		}
-		else
-		{
-			if(q[i].x<=mid) add(q[i].pos,q[i].k),q1[++cnt1]=q[i];
-			else q2[++cnt2]=q[i];
-		}
-	for(int i=1;i<=cnt1;i++)
-		if(q1[i].type==0) add(q1[i].pos,-q1[i].k); // Undo
-	for(int i=1;i<=cnt1;i++) q[ql+i-1]=q1[i];
-	for(int i=1;i<=cnt2;i++) q[ql+cnt1+i-1]=q2[i];
-	solve(l,mid,ql,ql+cnt1-1);
-	solve(mid+1,r,ql+cnt1,qr);
+
+void solve(int l, int r, int ql, int qr) {
+    if (l > r || ql > qr) return ;
+    if (l == r) {
+        rep(i, ql, qr) if (q[i].opt)
+            ans[q[i].pos] = l; 
+        return ;
+    } int cnt1 = 0, cnt2 = 0, mid = (l + r) >> 1;
+    rep(i, ql, qr) if (q[i].opt) {
+        re int tmp = ask(q[i].r) - ask(q[i].l - 1);
+        if (q[i].k <= tmp) q1[++ cnt1] = q[i];
+        else q[i].k -= tmp, q2[++ cnt2] = q[i];
+    } else {
+        if (q[i].l <= mid) add(q[i].pos, q[i].k), q1[++ cnt1] = q[i];
+        else q2[++ cnt2] = q[i];
+    } rep(i, 1, cnt1) if (!q1[i].opt) add(q1[i].pos, -q1[i].k);
+    rep(i, 1, cnt1) q[ql + i - 1] = q1[i];
+    rep(i, 1, cnt2) q[ql + cnt1 + i - 1] = q2[i];
+    solve(l, mid, ql, ql + cnt1 - 1), 
+    solve(mid + 1, r, ql + cnt1, qr); 
 }
+
 int a[N];
-int main()
-{
-	n=rd(),m=rd();
-	for(int i=1;i<=n;i++) {
-		a[i]=rd();
-		q[++cnt]=Query(a[i],0,1,i,0);
-	}
-	int tot=0;
-	for(int i=1;i<=m;i++)
-	{
-		char ch=getchar();while(ch!='Q'&&ch!='C')ch=getchar();
-		int x=rd(),y=rd(),z;
-		if(ch=='C') q[++cnt]=Query(a[x],0,-1,x,0),q[++cnt]=Query(a[x]=y,0,1,x,0);
-		else z=rd(),q[++cnt]=Query(x,y,z,++tot,1);
-	} solve(-INF,INF,1,cnt);
-	for(int i=1;i<=tot;i++)
-		printf("%d\n", ans[i]);
-	return 0;
+
+int main() {
+    n = rd(), m = rd();
+    rep(i, 1, n) a[i] = rd(), q[++ cnt] = (QUERY){a[i], 0, 1, i, 0};
+    int tot = 0; rep(i, 1, m) {
+        char c = getchar(); while (c != 'Q' && c != 'C') 
+            c = getchar();
+        int x = rd(), y = rd(), z; 
+        if (c == 'C') q[++ cnt] = (QUERY){ a[x], 0, -1, x, 0 },
+            q[++ cnt] = (QUERY){ a[x] = y, 0, 1, x, 0 };
+        else z = rd(), q[++ cnt] = (QUERY) { x, y, z, ++ tot, 1 };
+    } solve(-INF, INF, 1, cnt);
+    rep(i, 1, tot) printf("%d\n", ans[i]);
+    return 0;
 }
-/*
-5 3
-1 2 4 5 6
-Q 1 4 2
-C 1 3
-Q 1 4 2
-*/
